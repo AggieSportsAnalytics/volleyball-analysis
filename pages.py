f18ec2f0c3e0d2320b092b730c56e84a0202af40
Arnav Akula <34948408+arnavakula@users.noreply.github.com>
2024-05-15 15:15:48 -0700

    add basic graph display

--- pages.py
+++ pages.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def page_1(set_page):
     # st.markdown(
@@ -52,7 +54,7 @@ def page_1(set_page):
 
 def page_2(set_page):
     st.title("Choose a Team")
-    fp = 'raw/2016/team_game_by_game'
+    fp = 'raw/2016/team_stats'
 
     teams = []
 
@@ -62,12 +64,40 @@ def page_2(set_page):
     selected_team = st.selectbox("Select a team:", teams)
     if st.button("Next"):
         st.session_state.selected_team = selected_team
+        st.session_state.fp = f'{fp}/{st.session_state.selected_team}.csv'
         set_page(3)
         st.rerun()
 
 def page_3(set_page):
     st.title(f"Dashboard for {st.session_state.selected_team}")
+    df = pd.read_csv(st.session_state.fp)
     st.write("Here is the dashboard with stats and charts.")
+
+    col1, col2 = st.columns(2)
+
+    size = (16, 12)
+
+    with col1:
+        # Histogram of Player Heights
+        fig, ax = plt.subplots(figsize=size)
+        df['Pos'].value_counts().plot(kind='bar', color='orange', ax=ax)
+        ax.set_title('Count of Players by Position')
+        ax.set_xlabel('Position')
+        ax.set_ylabel('Count')
+        ax.set_xticks(ax.get_xticks())
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
+        ax.grid(axis='y')
+        st.pyplot(fig)
+    
+    with col2:
+        # Histogram of Player Heights
+        fig, ax = plt.subplots(figsize=size)
+        ax.hist(df['Ht'], bins=10, color='skyblue', edgecolor='black')
+        ax.set_title('Distribution of Player Heights')
+        ax.set_xlabel('Height (inches)')
+        ax.set_ylabel('Frequency')
+        ax.grid(True)
+        st.pyplot(fig)
 
     if st.button("Compare"):
         set_page(4)
