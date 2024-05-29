bd195f593b33ff7449eb2f2e120a6b68c7253acf
Arnav Akula <34948408+arnavakula@users.noreply.github.com>
2024-05-29 00:14:16 -0700

    display stat page after team selection

--- client/src/pages/StatDashboard.js
+++ client/src/pages/StatDashboard.js
@@ -1,17 +1,81 @@
 import React from 'react';
 import StatDashboardComponent from '../components/StatDashboard';
 import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function StatDashboard(props){
     const { state } = useLocation();
 
-    console.log(state['matchup'])
+    const yearA = state['matchup'][0]
+    const teamA = state['matchup'][1]
+    const yearB = state['matchup'][2]
+    const teamB = state['matchup'][3]
+
+    const [data, setData] = useState({});
+    const [rosterA, setRosterA] = useState([])
+    const [rosterB, setRosterB] = useState([])
+
+
+    useEffect(() => {
+        fetch(`/get_rosters?year_a=${yearA}&team_a=${teamA}&year_b=${yearB}&team_b=${teamB}`)
+        .then(response => response.json())
+        .then(data => {
+            setData(data);
+            setRosterA(data['rosters'][0]);
+            setRosterB(data['rosters'][1]);
+        })
+        .catch(error => {console.error('Error fetching graph:', error);});
+    }, [yearA, teamA, yearB, teamB]);
+
 
     return (
-        <div>
+        <>
             <StatDashboardComponent />
-        </div>
+            <h2>{teamA} ({yearA})</h2>
+            {Object.keys(data).length === 1 &&(
+                <table>
+                <thead>
+                    <tr>
+                        {Object.keys(rosterA[0]).map((key, index) => (
+                            <th key={index}>{key}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {rosterA.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {Object.values(row).map((value, colIndex) => (
+                                <td key={colIndex}>{value}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            )}
+            <h2>{teamB} ({yearB})</h2>
+            {Object.keys(data).length === 1 &&(
+                <table>
+                <thead>
+                    <tr>
+                        {Object.keys(rosterB[0]).map((key, index) => (
+                            <th key={index}>{key}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {rosterB.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {Object.values(row).map((value, colIndex) => (
+                                <td key={colIndex}>{value}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            )}
+        </>
     );
+
 };
 
 export default StatDashboard;
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import os
 import json
+import matplotlib.pyplot as plt
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,15 +12,28 @@ def members():
 
 @app.route('/get_schools')
 def get_schools():
-
-    with open('data/schools.json', 'r') as fp:
-        schools = json.load(fp)
+    with open('data/schools.json', 'r') as f:
+        schools = json.load(f)
     
     year = request.args.get('year')
     
     return schools[year]
-    
 
+@app.route('/get_rosters')
+def get_rosters():
+    year_a = request.args.get('year_a')
+    team_a = request.args.get('team_a')
+    year_b = request.args.get('year_b')
+    team_b = request.args.get('team_b')
+
+    with open('data/rosters.json', 'r') as f:
+        rosters = json.load(f)
+    
+    print(rosters[year_a][team_a])
+    
+    return {'rosters': [rosters[year_a][team_a], rosters[year_b][team_b]]}
+        
+    
 
 
 if __name__ == '__main__':
